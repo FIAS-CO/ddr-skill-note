cbb0cf7f3f7ceb83e5c78ccf14d3e3e9e807e9cc
sig-ume <beyondthemoonface@gmail.com>
2024-09-10 12:05:13 +0900

    フォーマッター

--- src/components/RankingSongRow.tsx
+++ src/components/RankingSongRow.tsx
@@ -2,14 +2,14 @@ import BaseSongRow from "./BaseSongRow";
 import { BaseSong } from "../types/song";
 
 export interface RankingSong extends BaseSong {
-    overallPercentage: number;
-  }
-  
-  export const RankingSongRow: React.FC<{ song: RankingSong; rank: number }> = ({ song, rank }) => (
-    <BaseSongRow<RankingSong>
-      song={song}
-      renderCells={(song) => [
-        <span className="font-medium">{rank}</span>,
+  overallPercentage: number;
+}
+
+export const RankingSongRow: React.FC<{ song: RankingSong; rank: number }> = ({ song, rank }) => (
+  <BaseSongRow<RankingSong>
+    song={song}
+    renderCells={(song) => [
+      <span className="font-medium">{rank}</span>,
       <span className="font-medium text-gray-900">{song.title}</span>,
       <span className="text-gray-600">{song.level}</span>,
       <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
@@ -20,4 +20,3 @@ export interface RankingSong extends BaseSong {
   />
 );
 
-  
