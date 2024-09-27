c8dfae3cb8092d249d4031dbad754dcef866bdac
sig-ume <beyondthemoonface@gmail.com>
2024-09-27 23:14:09 +0900

    スキル帳ページ微修正

--- src/components/PersonalSkillPage.tsx
+++ src/components/PersonalSkillPage.tsx
@@ -88,13 +88,12 @@ const PersonalSkillPage: React.FC = () => {
         <div className="w-10"> {/* 左側の空白スペース */}</div>
         <h1 className="text-2xl sm:text-3xl font-bold text-center flex-grow">
           {userName} さんの
-          {isMobile ? <br /> : ' '}
+          {isMobile ? <br /> : ''}
           フレアスキル帳
         </h1>
-
         <button
           onClick={shareToX}
-          className="flex items-center bg-blue-400 hover:bg-blue-500 text-white font-bold py-1 px-2 rounded">
+          className={`flex items-center bg-blue-400 hover:bg-blue-500 text-white font-bold py-1 ${!isMobile ? 'px-2' : 'px-1'} rounded`}>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" className="mr-2">
             <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z" />
           </svg>
